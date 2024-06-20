d8a3157479b59d7adc24e36424996b6238c542a5
Vinicius wessner <67404041+ViniciusWessner@users.noreply.github.com>
2024-06-20 10:10:23 -0300

    Created fragments TODO  and configration viewbinding

---
+++ app/src/main/java/com/example/taskapp/ui/TodoFragment.kt
@@ -0,0 +1,28 @@
+package com.example.taskapp.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.taskapp.R
+import com.example.taskapp.databinding.FragmentHomeBinding
+import com.example.taskapp.databinding.FragmentTodoBinding
+
+class TodoFragment : Fragment() {
+
+    private var _binding: FragmentTodoBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentTodoBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
