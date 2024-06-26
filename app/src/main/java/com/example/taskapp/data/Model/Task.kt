ef8502df0034b9d6ccf7efd3b8dcf45618f06b3f
Vinicius wessner <67404041+ViniciusWessner@users.noreply.github.com>
2024-06-26 09:15:06 -0300

    Create adapter taskitem

---
+++ app/src/main/java/com/example/taskapp/data/Model/Task.kt
@@ -0,0 +1,13 @@
+package com.example.taskapp.data.Model
+
+import android.app.ActivityManager.TaskDescription
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+//usamos a data class pra jogar dados em memoria de uma classe para outra etc
+
+@Parcelize
+data class Task(
+    val id: String,
+    val description: String
+):Parcelable
