0374b13c8d72def8f16ff722543a70764deda46a
Vinicius wessner <67404041+ViniciusWessner@users.noreply.github.com>
2024-06-26 23:02:40 -0300

    Created to recyclerView for fragments and tasklisk screen

--- app/src/main/java/com/example/taskapp/ui/DoingFragment.kt
+++ app/src/main/java/com/example/taskapp/ui/DoingFragment.kt
@@ -5,14 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.taskapp.R
+import com.example.taskapp.data.Model.Status
+import com.example.taskapp.data.Model.Task
 import com.example.taskapp.databinding.FragmentDoingBinding
 import com.example.taskapp.databinding.FragmentTodoBinding
+import com.example.taskapp.ui.adapter.TaskAdapter
 
 class DoingFragment : Fragment() {
 
     private var _binding: FragmentDoingBinding? = null
     private val binding get() = _binding!!
+    private lateinit var taskAdapter: TaskAdapter
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,6 +27,26 @@ class DoingFragment : Fragment() {
         _binding = FragmentDoingBinding.inflate(inflater, container, false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initReciclerViewTasks(getTasks())
+    }
+
+    private fun initReciclerViewTasks(task: List<Task>){
+        //inicializando adapter
+        taskAdapter = TaskAdapter(task)
+
+        binding.rvTask.layoutManager = LinearLayoutManager(requireContext())
+        binding.rvTask.setHasFixedSize(true)
+        binding.rvTask.adapter = taskAdapter
+    }
+
+    private fun getTasks() = listOf<Task>(
+        Task("1", "Criar e configurar meu primeiro app", Status.DOING),
+        Task("2", "Configurar pipeline github actions", Status.DOING),
+        Task("3", "Analisar graficos do grafana", Status.DOING)
+    )
 
     override fun onDestroyView() {
         super.onDestroyView()
