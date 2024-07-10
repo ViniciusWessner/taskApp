2c91f550cdcdc01f8cc503bcddf800cb13476809
Vinicius wessner <67404041+ViniciusWessner@users.noreply.github.com>
2024-07-10 09:12:28 -0300

    Configure firebase auth

--- app/src/main/java/com/example/taskapp/ui/SplashFragment.kt
+++ app/src/main/java/com/example/taskapp/ui/SplashFragment.kt
@@ -10,10 +10,16 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.taskapp.R
 import com.example.taskapp.databinding.FragmentSplashBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class SplashFragment : Fragment() {
     private var _binding: FragmentSplashBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -24,13 +30,19 @@ class SplashFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        auth = Firebase.auth
         Handler(Looper.getMainLooper()).postDelayed(this::checkAuth,3000)
 
     }
 
     private fun checkAuth() {
-        findNavController().navigate(R.id.action_splashFragment_to_auth)
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
+        } else {
+            findNavController().navigate(R.id.action_splashFragment_to_auth)
+        }
+
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/taskapp/ui/auth/RecoverAccountFragment.kt
+++ app/src/main/java/com/example/taskapp/ui/auth/RecoverAccountFragment.kt
@@ -1,22 +1,30 @@
 package com.example.taskapp.ui.auth
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.navigation.fragment.findNavController
 import com.example.taskapp.R
 import com.example.taskapp.databinding.FragmentRecoverAccountBinding
 import com.example.taskapp.util.initToolbar
 import com.example.taskapp.util.showButtomSheet
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 
 class RecoverAccountFragment : Fragment() {
 
     private var _binding: FragmentRecoverAccountBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -28,6 +36,7 @@ class RecoverAccountFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initToolbar(binding.toolbar)
+        auth = Firebase.auth
         initListeners()
     }
 
@@ -42,10 +51,25 @@ class RecoverAccountFragment : Fragment() {
         val email = binding.editEmail.text.toString().trim()
 
         if (email.isNotEmpty()){
-            Toast.makeText(requireContext(), "Link de redefinicao encaminhado", Toast.LENGTH_SHORT).show()
+            binding.progressBar.isVisible = true
+            recoveryUserAccount(email)
         }else{
             showButtomSheet(massage = getString(R.string.fragment_account_massage_warning))
         }
+    }
+
+    private fun recoveryUserAccount(email: String){
+        auth.sendPasswordResetEmail(email)
+            .addOnCompleteListener { task ->
+                binding.progressBar.isVisible = false
+                if (task.isSuccessful){
+                    showButtomSheet(
+                        massage = getString(R.string.fragment_account_massage),
+                    )
+                }else {
+                    Toast.makeText(requireContext(), task.exception?.message, Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 
     override fun onDestroyView() {
